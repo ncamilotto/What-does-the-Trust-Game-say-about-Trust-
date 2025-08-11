da666ceb8668f4c44720ac0e2a59b2933a673fb3
Camilotto Nicolas <ncamilotto@gmail.com>
2025-08-11 16:24:46 +0200

    add create_database

---
+++ create_database.R
@@ -0,0 +1,17 @@
+library(bibliometrix)
+library(tibble)
+library(dplyr)
+
+Sys.getenv("MY_FOLDER_PATH")
+
+folder_path <- Sys.getenv("MY_FOLDER_PATH")
+
+files <- list.files(path = folder_path, pattern = "\\.txt$", full.names = TRUE)
+
+# Appliquer convert2df sur chaque fichier, puis fusionner en un seul dataframe
+
+database_WOS <- files %>%
+  lapply(function(f) convert2df(f, dbsource = "wos", format = "plaintext")) %>%
+  bind_rows() %>%
+  rownames_to_column(var = "REF") %>%
+  select(REF, AU, CR, DT, PY, SC, PT, SO, TI, SR, Z9, WC, AB)
